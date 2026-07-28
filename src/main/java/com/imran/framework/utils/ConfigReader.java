0cdb67d094a0af4218e283e08f4229837fd4c29d
imranjss <95536776+imranjss@users.noreply.github.com>
2026-07-28 15:58:01 +0530

    Create ConfigReader.java

---
+++ src/main/java/com/imran/framework/utils/ConfigReader.java
@@ -0,0 +1,33 @@
+package com.imran.framework.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigReader {
+    private static final Properties properties = new Properties();
+
+    static {
+        try (InputStream input = ConfigReader.class.getClassLoader()
+                .getResourceAsStream("config.properties")) {
+            if (input == null) {
+                throw new RuntimeException("config.properties not found in src/test/resources");
+            }
+            properties.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load config.properties", e);
+        }
+    }
+
+    public static String get(String key) {
+        return properties.getProperty(key);
+    }
+
+    public static boolean getBoolean(String key) {
+        return Boolean.parseBoolean(get(key));
+    }
+
+    public static long getLong(String key) {
+        return Long.parseLong(get(key));
+    }
+}
